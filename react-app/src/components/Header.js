c8b31fe2f3365232a5e0610ff86b9549bd199324
Joshua Coleman <procoleman77@outlook.com>
2020-01-06 12:59:30 -0500

    added header component and styles

---
+++ react-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <i class='fas fa-book-reader'></i>
+      <h1>BookCards</h1>
+    </header>
+  );
+};
+
+export default Header;
